824e4ee315221f9f60693530e14e0fca7b45f01a
bulhard <pravoslav.milenkov@gmail.com>
2021-10-04 12:33:57 +0300

    Initial commit

---
+++ Presentations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Presentations
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Presentation");
+        }
+    }
+}
