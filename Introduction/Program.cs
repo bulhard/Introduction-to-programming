c990bce7fac20c2f4510535cadb24229e569ebbb
bulhard <pravoslav.milenkov@gmail.com>
2021-10-04 20:13:14 +0300

    Sumator Windows Forms

--- Introduction/Program.cs
+++ Introduction/Program.cs
@@ -2,11 +2,11 @@
 
 namespace Introduction
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello C#");
         }
     }
 }
---
+++ SumatorWinForm/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SumatorWinForm
+{
+    public partial class FormCalculate : Form
+    {
+        public FormCalculate()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            int numA = int.Parse(textBox1.Text);
+            int numB = int.Parse(textBox2.Text);
+
+            int sum = numA + numB;
+
+            textBoxSum.Text = sum.ToString();
+        }
+    }
+}
